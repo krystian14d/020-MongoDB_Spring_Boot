dab3fd1fd73676f9cb3f55150dd5aa70ca8b82c8
Krystian <krystian14d@gmail.com>
2022-07-22 12:50:21 +0200

    test data load into db implemented

---
+++ init-mongodb/01-create-db.js
@@ -0,0 +1,3 @@
+// creating database and collections:
+db = new Mongo().getDB("testdb1");
+db.createCollection("transactions", {capped: false});
--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.amigoscode.demo;
 
+import com.amigoscode.demo.dbDataLoader.TransactionDataLoader;
+import com.amigoscode.demo.transaction.Transaction;
+import com.amigoscode.demo.transaction.TransactionRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(TransactionRepository transactionRepository){
+        return args -> {
+            TransactionDataLoader transactionDataLoader = new TransactionDataLoader();
+            List<Transaction> transactions = transactionDataLoader.readCsv();
+            transactionRepository.saveAll(transactions);
+        };
     }
 
 }
